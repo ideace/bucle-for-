498915853c1186fc1e6c1706f7e533e293e6eaec
ivand <ivandariorap1@outlook.es>
2022-07-07 20:43:21 -0500

    bucle for

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for(var numeroFor = 0; numeroFor <= 3; numeroFor++){
+        System.out.println(numeroFor);
+    }
+    }
+}
